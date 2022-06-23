66b7779675764bc19fe169a8d8bc2098ec6b2795
Roman Aptekar <102629159+Lopuhon@users.noreply.github.com>
2022-06-23 23:41:08 +0300

    add NOD games, add new README

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -5,12 +5,14 @@ public class App {
     public static String greet = "1 - Greet";
     public static String even = "2 - Even";
     public static String calc = "3 - Calc";
+    public static String nod = "4 - NOD";
     public static void main(String[] args) {
         System.out.println("Please enter the game " +
                 "number and press Enter.\n" +
                 greet+ "\n" +
                 even + "\n" +
                 calc + "\n" +
+                nod +"\n" +
                 "0 - Exit\n" +
                 "Your choice: ");
         Scanner sc = new Scanner(System.in);
@@ -19,6 +21,7 @@ public class App {
             case 1 -> Cli.greeting();
             case 2 -> Even.startGame();
             case 3 -> Calc.startGame();
+            case 4 -> NOD.startGame();
             case 0 -> System.out.println("Exit");
         }
     }
---
+++ src/main/java/hexlet/code/NOD.java
@@ -0,0 +1,32 @@
+package hexlet.code;
+import  java.lang.String;
+public class NOD {
+
+    public static final int NUMBER_RANDOM = 100;
+    public static final int ARRAYS_LENGTH = 3;
+
+    public static void startGame(){
+        Engine.start("Find the greatest common divisor of given numbers.", generateRoundData());
+    }
+    public static int gcd(int a, int b) {
+        while (a != 0 && b != 0) {
+            if( a > b) {
+                a = a % b;
+            } else {
+                b = b % a;
+            }
+        }
+        return a + b;
+    }
+    public static String[][] generateRoundData() {
+        String[][] roundsData = new String[ARRAYS_LENGTH][2];
+        for(int i = 0; i < ARRAYS_LENGTH; i++) {
+            int firstNum = Utils.generateRandomNumber(NUMBER_RANDOM);
+            int secondNum = Utils.generateRandomNumber(NUMBER_RANDOM);
+            var question = firstNum+ " " + secondNum;
+            var answer = String.valueOf(gcd(firstNum, secondNum));
+            roundsData[i] = new String[]{question, answer};
+        }
+        return  roundsData;
+}
+}
