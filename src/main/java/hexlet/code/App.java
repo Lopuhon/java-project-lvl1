9d52622de3497b4cd48f08fd2d8d65a93dcc8c13
Roman Aptekar <102629159+Lopuhon@users.noreply.github.com>
2022-07-14 00:02:04 +0300

    add change app

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -15,27 +15,45 @@ public class App {
     private static String nod = "4 - NOD";
     private static String progression = "5 - progression";
     private static String prime = "6 - Prime number";
+
+    public static final int GREET_NUMBER = 1;
+    public static final int EVEN_NUMBER = 2;
+    public static final int CALC_NUMBER = 3;
+    public static final int NOD_NUMBER = 4;
+    public static final int PROGRESSION_NUMBER = 5;
+    public static final int PRIME_NUMBER = 6;
+    public static final int EXIT_NUMBER = 0;
     public static void main(String[] args) {
-        System.out.println("Please enter the game " +
-                "number and press Enter.\n" +
-                greet+ "\n" +
-                even + "\n" +
-                calc + "\n" +
-                nod +"\n" +
-                progression + "\n" +
-                prime + "\n" +
-                "0 - Exit\n" +
+        System.out.println("Please enter the game "
+                +
+                "number and press Enter.\n"
+                +
+                greet
+                + "\n" +
+                even
+                + "\n" +
+                calc
+                + "\n" +
+                nod
+                +"\n" +
+                progression
+                + "\n" +
+                prime
+                + "\n" +
+                "0 - Exit\n"
+                +
                 "Your choice: ");
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         switch (a) {
-            case 1 -> Cli.greeting();
-            case 2 -> Even.startGame();
-            case 3 -> Calc.startGame();
-            case 4 -> NOD.startGame();
-            case 5 -> Progression.startGame();
-            case 6 -> PrimeNumber.startGame();
-            case 0 -> System.out.println("Exit");
+            case GREET_NUMBER -> Cli.greeting();
+            case EVEN_NUMBER -> Even.startGame();
+            case CALC_NUMBER -> Calc.startGame();
+            case NOD_NUMBER -> NOD.startGame();
+            case PROGRESSION_NUMBER -> Progression.startGame();
+            case PRIME_NUMBER -> PrimeNumber.startGame();
+            case EXIT_NUMBER -> System.out.println("Exit");
+            default -> System.out.println(" No more option. Try again");
         }
     }
 }
