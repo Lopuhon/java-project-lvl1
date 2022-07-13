8d9a21035170a446251934446306c0b6efa1f224
Roman Aptekar <102629159+Lopuhon@users.noreply.github.com>
2022-07-13 22:50:37 +0300

    add Prime game

--- src/main/java/hexlet/code/games/PrimeNumber.java
+++ src/main/java/hexlet/code/games/PrimeNumber.java
@@ -8,7 +8,7 @@ public class PrimeNumber {
     public static final int LIMIT_LENGTH_ARRAYS = 3;
 
     public static void startGame() {
-        Engine.start("Answer 'yes' if given number is prime. Otherwise answer 'no'.",generateRoundData());
+        Engine.start("Answer 'yes' if given number is prime. Otherwise answer 'no'.", generateRoundData());
     }
 
     public static boolean isNumberPrime(int number) {
@@ -23,14 +23,16 @@ public class PrimeNumber {
             return false;
         }
     }
+
     private static String[][] generateRoundData() {
         String[][] roundsData = new String[LIMIT_LENGTH_ARRAYS][2];
         for (int i = 0; i < LIMIT_LENGTH_ARRAYS; i++) {
             int number = Utils.generateRandomNumber(RANDOM);
             var answer = isNumberPrime(number) ? "yes" : "no";
             var question = String.valueOf(number);
-            roundsData[i] = new String[] {question, answer};
+            roundsData[i] = new String[]{question, answer};
         }
         return roundsData;
     }
 }
+
